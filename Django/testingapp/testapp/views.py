f31b52d58640204435d274f4f8a15d41c3bf8fe6
Liuda Prysiazhna <liudaprysiazhna@gmail.com>
2018-09-13 21:46:46 +0300

    All together

--- Django/testingapp/testapp/views.py
+++ Django/testingapp/testapp/views.py
@@ -1,12 +1,18 @@
-# howdy/views.py
+
+# Create your views here.
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
 
 # Add this view
-class AboutPageView(TemplateView):
-    template_name = "about.html"
+class TestPageView(TemplateView):
+    template_name = "test.html"
+
+
 
