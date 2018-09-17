0a0ea8afb173abca53d6f2ed802cd9c2ff065f31
Liuda Prysiazhna <liudaprysiazhna@gmail.com>
2018-09-17 13:43:32 +0300

    testingapp finished

--- Django/testingapp/testapp/views.py
+++ Django/testingapp/testapp/views.py
@@ -5,13 +5,14 @@ from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import pandas as pd
-import datetime
+
 from random import shuffle
 
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
         return render(request, 'index.html', context=None)
+
 
 class ResultPageView(TemplateView):
     def get(self, request, **kwargs):
@@ -30,6 +31,7 @@ class ResultPageView(TemplateView):
 
         return render(request, 'result.html', {'text_legend':text_legend, 'text':text, 'text_button':text_button,'snake':snake})
 
+
 class TestPageView(TemplateView):
     def get(self, request, **kwargs):
         text_1 = request.GET['TEXT_1']
@@ -41,7 +43,6 @@ class TestPageView(TemplateView):
 
         test_data = {'aaa': text_1, 'question': test_question[0][0], 'val1':test_question[0][1],'val2':test_question[0][2],'val3': test_question[0][3],'answer_r': test_question[0][4]}
         return render(request, 'test.html', test_data)
-
 
 
 class Test(object):
@@ -59,41 +60,4 @@ class Test(object):
     def question(self):
         test = self.questions.values[self.random_question()]
         return test
-
-    def ask_questions(self):
-        user_score = 0
-        result = ''
-        user_test = []
-
-        for i in range(self.n):
-
-            test = self.questions.values[self.random_question()]
-            user_answer = input(str(test[i][0]) + '\nВаріанти відповідей: ' + str(test[i][1:4]) + '\nВаша відповідь: ')
-            print()
-
-            user_test.append(list(test[i]))
-            user_test[i].append(user_answer)
-
-            if user_answer == str(test[i][4]):
-                user_score += 1
-
-        result = f'Ви набрали {user_score} з {self.n} балів!'
-        return result, user_test
-
-    def save_user_test(self, user_name, user_test):
-        date = datetime.datetime.today().strftime('%Y-%m-%d')
-        header = ['Питання', 'Варіант 1', 'Варіант 2', 'Варіант 3', 'Відповідь', 'Відповідь юзера']
-        filename = user_name + '_' + date + '.csv'
-        df_user_test = pd.DataFrame(user_test)
-        df_user_test.to_csv(filename, header=header)
-
-    def start_test(self):
-        user_name = input('Ваше ім\'я: ')
-        result, user_test = self.ask_questions()
-        self.save_user_test(user_name, user_test)
-        print(result)
-
-
-
-
 
